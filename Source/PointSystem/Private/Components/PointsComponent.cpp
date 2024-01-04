f8edb72232672d4df4b0aafa819e13dfc66fc116
Spacemex <devenshona@gmail.com>
2024-01-03 21:40:07 -0600

    Add PointSystem plugin with PointsComponent and basic functionalities
    This commit introduces the PointSystem plugin with a PointsComponent. It establishes the structure for handling and manipulating points within the game, with relevant getters and setters along with add/remove/check points functions. The necessary build configurations and module interactions are also defined.

---
+++ Source/PointSystem/Private/Components/PointsComponent.cpp
@@ -0,0 +1,43 @@
+﻿/*
+ * Copyright (c) 2023 Space_Mex
+ * 
+ * All rights reserved.
+ * 
+ * This file is part of the PointsSystem Plugin.
+ * Unauthorized copying of this file, via any medium, is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+#include "Components/PointsComponent.h"
+
+
+// Sets default values for this component's properties
+UPointsComponent::UPointsComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPointsComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPointsComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                     FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/PointSystem/Public/Components/PointsComponent.h
@@ -0,0 +1,75 @@
+﻿/*
+ * Copyright (c) 2023 Space_Mex
+ * 
+ * All rights reserved.
+ * 
+ * This file is part of the PointsSystem Plugin.
+ * Unauthorized copying of this file, via any medium, is strictly prohibited.
+ * Proprietary and confidential.
+ */
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "PointsComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class POINTSYSTEM_API UPointsComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UPointsComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	//Variables
+	
+	/**This Is The Starting Points What Your Default Points When The Game Begins (Default is 500)*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "PointsSystem | Default | Points")
+	int StartingPoints = 500;
+
+	/**This Is Where Your Total Points Are Stored (Default is StartingPoints)*/
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "PointsSystem | Default | Points")
+	int Points = StartingPoints;
+
+	//Functions
+
+	/**Set The Points variable (Setter)*/
+	UFUNCTION(BlueprintCallable, Category = "PointsSystem | Setters")
+	void SetTotalPoints(int NewPoints){Points = NewPoints;}
+
+	/**Set The Starting Points (Setter)*/
+	UFUNCTION(BlueprintCallable, Category = "PointsSystem | Setters")
+	void SetStartingPoints(int NewPoints){StartingPoints = NewPoints;}
+
+	/**Get The Current Total Points (Getter)*/
+	UFUNCTION(BlueprintCallable, Category = "PointsSystem | Getters")
+	int GetTotalPoints(){return Points;}
+
+	/**Get Starting Points (Getter)*/
+	UFUNCTION(BlueprintCallable, Category = "PointsSystem | Getters")
+	int GetStartingPoints(){return StartingPoints;}
+
+	/**Add Points To Current Points*/
+	UFUNCTION(BlueprintCallable, Category = "PointsSystem | Adders")
+	void AddPoints(int Value){Points += Value;}
+
+	/**Remove Points From Current Points*/
+	UFUNCTION(BlueprintCallable, Category = "PointsSystme | Removers")
+	void RemovePoints(int Value){if (Points > 0){Points = FMath::Clamp(Points - Value,0, MAX_int32);}}
+
+	/**Has Enough Point*/
+	UFUNCTION(BlueprintCallable, Category = "PointsSytem | Checks")
+	bool HasEnoughPoints(int Value){if (Points >= Value){return true;}return false;}
+};
